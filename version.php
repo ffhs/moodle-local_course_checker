a0237d4edbcaad2a7598bffa19b48e1b89492363
Simon Gisler <simon.gisler@ffhs.ch>
2025-11-13 15:01:00 +0100

    updaet changes.md

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025061000;
+$plugin->version = 2025111300;
 $plugin->requires = 2024042203;
 $plugin->release = 'v4.4-r1';
 $plugin->component = 'local_course_checker';
